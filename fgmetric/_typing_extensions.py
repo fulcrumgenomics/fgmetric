1311244134fbda7fffc2509e5e28dbd660c8b41a
Matt Stone <matt@fulcrumgenomics.com>
2025-09-03 13:07:19 -0400

    feat: typing helpers

---
+++ fgmetric/_typing_extensions.py
@@ -0,0 +1,73 @@
+from types import GenericAlias
+from types import UnionType
+from typing import Union
+from typing import get_args
+from typing import get_origin
+
+type TypeAnnotation = type | UnionType | GenericAlias
+"""
+A type annotation may be any of the following:
+    1) `type`, when declaring any of the built-in Python types
+    2) `types.UnionType`, when declaring union types using PEP604 syntax (e.g. `int | None`)
+    3) `types.GenericAlias`, when declaring generic collection types using PEP 585 syntax (e.g.
+       `list[int]`)
+"""
+
+TYPE_ANNOTATION_TYPES = (type, UnionType, GenericAlias)
+
+
+def is_optional(annotation: TypeAnnotation) -> bool:
+    """
+    Check if a type is `Optional`.
+
+    An optional type may be declared using three syntaxes: `Optional[T]`, `Union[T, None]`, or
+    `T | None`. All of these syntaxes are supported by this function.
+
+    For simplicity, this function does not permit `T` to be a union type.
+
+    Args:
+        annotation: A type annotation.
+
+    Returns:
+        True if the type is a union type with exactly two elements, one of which is `None`.
+        False otherwise.
+
+    Raises:
+        TypeError: If the input is not a valid `TypeAnnotation` type.
+    """
+    origin = get_origin(annotation)
+    args = get_args(annotation)
+
+    # Optional[T] and Union[T, None] have `typing.Union` as its origin.
+    # PEP604 syntax (`T | None`) has `types.UnionType` as its origin.
+    return (
+        origin is not None
+        and (origin is Union or origin is UnionType)
+        and len(args) == 2
+        and type(None) in args
+    )
+
+
+def unpack_optional(annotation: TypeAnnotation) -> TypeAnnotation:
+    """Retrieve the parameterized type of the Optional."""
+    if not is_optional(annotation):
+        raise ValueError(f"Type is not Optional: {annotation}")
+
+    args = [t for t in get_args(annotation) if t is not type(None)]
+    type_parameter = args[0]
+
+    assert isinstance(type_parameter, TYPE_ANNOTATION_TYPES)  # type narrowing
+
+    return type_parameter
+
+
+def is_list(annotation: TypeAnnotation | None) -> bool:
+    """True if the type annotation is not None and a list type."""
+    if annotation is None:
+        return False
+    elif get_origin(annotation) is list:
+        return True
+    elif is_optional(annotation) and get_origin(unpack_optional(annotation)) is list:
+        return True
+    else:
+        return False
---
+++ tests/test_typing_extensions.py
@@ -0,0 +1,68 @@
+from typing import Optional
+from typing import Union
+
+import pytest
+
+from fgmetric._typing_extensions import TypeAnnotation
+from fgmetric._typing_extensions import is_list
+from fgmetric._typing_extensions import is_optional
+from fgmetric._typing_extensions import unpack_optional
+
+
+@pytest.mark.parametrize(
+    "annotation",
+    [
+        Union[str, None],
+        Optional[str],
+        str | None,
+    ],
+)
+def test_is_optional(annotation: TypeAnnotation) -> None:
+    """Should identify optional union types."""
+    assert is_optional(annotation)
+
+
+@pytest.mark.parametrize(
+    "annotation",
+    [
+        str,
+        Union[str, int],
+        Union[str, int, None],
+        str | int,
+        str | int | None,
+    ],
+)
+def test_is_not_optional(annotation: TypeAnnotation) -> None:
+    """Should reject non-optional or non-union types."""
+    assert not is_optional(annotation)
+
+
+def test_unpack_optional() -> None:
+    """Should retrieve the parameterized type."""
+    assert unpack_optional(int | None) is int
+
+
+@pytest.mark.parametrize(
+    "annotation",
+    [
+        list[int],
+        list[str],
+        Optional[list[str]],
+        list[str] | None,
+    ],
+)
+def test_is_list(annotation: TypeAnnotation) -> None:
+    """Should identify lists, even within an Optional."""
+    assert is_list(annotation)
+
+
+@pytest.mark.parametrize(
+    "annotation",
+    [
+        str,
+        str | None,
+    ],
+)
+def test_is_not_list(annotation: TypeAnnotation) -> None:
+    """Should reject non-list types."""
+    assert not is_list(annotation)
