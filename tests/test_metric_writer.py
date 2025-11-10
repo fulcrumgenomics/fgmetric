4ecf2d91e080380b43a7b9b46f8490753d98cc6a
Matt Stone <matt@fulcrumgenomics.com>
2025-11-10 08:33:41 -0500

    feat: MetricWriter
    doc: docstring
    
    chore: make fieldnames private
    
    chore: ruff MetricWriter

---
+++ tests/test_metric_writer.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import assert_type
+
+import pytest
+
+from fgmetric import Metric
+from fgmetric import MetricWriter
+
+
+class FakeMetric(Metric):
+    """A fake Metric to use in tests."""
+
+    foo: str
+    bar: int
+
+
+def test_writer(tmp_path: Path) -> None:
+    """Test we can write a Metric to file."""
+    fpath = tmp_path / "test.txt"
+
+    writer: MetricWriter[FakeMetric]
+    with MetricWriter(FakeMetric, fpath) as writer:
+        assert_type(writer, MetricWriter[FakeMetric])
+        writer.write(FakeMetric(foo="abc", bar=1))
+        writer.write(FakeMetric(foo="def", bar=2))
+
+    with fpath.open("r") as f:
+        assert next(f) == "foo\tbar\n"
+        assert next(f) == "abc\t1\n"
+        assert next(f) == "def\t2\n"
+        with pytest.raises(StopIteration):
+            next(f)
